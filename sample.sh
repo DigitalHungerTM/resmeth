9590b0ace25990687613f0245aee6750fec4f79a
DigitalHungerTM <m.afman.2@student.rug.nl>
2022-03-15 11:23:33 +0100

    first version of 'de' counter

---
+++ sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# descr: this is a simple script that counts
+#	 that counts the number of occurances
+#	 of the word 'de' case insensitively
+#
+# usage: ./sample.sh file.txt
+
+# test if a file is specified as the first argument
+TEXT=$1
+if [ -z "$TEXT" ]; then
+	echo "Usage: ./sample.sh file.txt"
+	exit
+fi
+
+# cat the file, find 'de', count
+cat $1 |
+grep -w -o -i 'de' |
+wc -l
