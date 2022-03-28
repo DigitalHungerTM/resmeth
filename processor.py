f5d634a0fcd5f342acb4f908428cd7cbfbfa3e42
DigitalHungerTM <m.afman.2@student.rug.nl>
2022-03-29 01:58:39 +0200

    added pretty and pure formatting and a plotting option

--- processor.py
+++ processor.py
@@ -1,16 +1,43 @@
 import sys
+import matplotlib.pyplot as plt
 
 
-def open_file(filename):
-    with open(filename, "r") as inp:
-        data = inp.read()
-        data = data.strip()  # strip trailing and leading spaces to prevent errors
+def arg_check(argv):
+    usage = f"Usage: python3 {argv[0]} filenames.txt pure|extended|plot"
+    if len(argv) != 3:
+        print("Error, there are too few/many arguments")
+        print(usage)
+        exit(-1)
+    elif argv[2] != "pure" and argv[2] != "extended" and argv[2] != "plot":
+        print("Error: second argument should either be 'pure', 'extended' or 'plot'")
+        exit(-1)
 
-    return data
+
+def open_filenames(filename):
+    with open(filename) as filenames_input:
+        filenames = filenames_input.readlines()
+
+    filenames = ["data/" + filename.strip() for filename in filenames]
+    return filenames
 
 
 def split_to_chunks(data):
+    """takes a string of data seperated by double newlines and splits it into chunks of data,
+       returns a list of strings"""
     chunks = data.split("\n\n")
+    return chunks
+
+
+def open_files(filenames):
+    """takes a list of filenames and returns a list of chunks"""
+    chunks = []
+    for file in filenames:
+        with open(file, "r") as inp:
+            data = inp.read().strip()
+
+        for chunk in split_to_chunks(data):
+            chunks.append(chunk)
+        # chunks = [chunk for chunk in split_to_chunks(data)]
     return chunks
 
 
@@ -22,29 +49,98 @@ def split_into_values(chunk):
     return values
 
 
-def main():
-    data = open_file(sys.argv[1])
-    chunks = split_to_chunks(data)
-    value_list_list = [split_into_values(chunk) for chunk in chunks]
-    print(value_list_list)
+def combine_values(value_list_list):
+    combined_list = []
+    for hour in range(24):
+        hour = str(hour)
+        if len(hour) != 2:
+            hour = "0" + hour
 
-    for i in range(24):
-        i = str(i)
-        if len(i) != 2:
-            i = "0" + i
-
-        total_tweets = 0
-        laughter_tweets = 0
+        hour_total_tweets = 0
+        hour_laughter_tweets = 0
         for value_list in value_list_list:
-            if value_list[0] == i:
-                total_tweets += value_list[2]
-                laughter_tweets += value_list[1]
+            if value_list[0] == hour:
+                hour_laughter_tweets += value_list[1]
+                hour_total_tweets += value_list[2]
 
-        print(f"hour {i}")
-        # print(f"total:    {total_tweets}")
-        # print(f"laughter: {laughter_tweets}")
-        print(f"ratio:    {laughter_tweets/total_tweets*100:.2f}%")
+        combined_list.append([hour, hour_laughter_tweets, hour_total_tweets])
+
+    return combined_list
+
+
+def print_pure(combined_list):
+    """sums up the values that have the same hour"""
+    print("hour,laughter,total,ratio,percentage")
+    for value_list in combined_list:
+        hour = value_list[0]
+        laughter = value_list[1]
+        total = value_list[2]
+        if total == 0:
+            total += 1
+        ratio = laughter/total
+        percentage = laughter/total*100
+        if total == 1:
+            total -= 1
+        print(f"{hour},{laughter},{total},{ratio:.4f},{percentage:.2f}")
+
+
+def print_extended(combined_values):
+    """prints a pretty summary of the collected data"""
+    for value_list in combined_values:
+        hour = value_list[0]
+        laughter = value_list[1]
+        total = value_list[2]
+        if total == 0:
+            total += 1  # to prevent ZeroDivisionError
+        ratio = laughter/total
+        percentage = laughter/total*100
+        if total == 1:
+            total -= 1
+        print(f"hour:          {hour}")
+        print(f"laughter:      {laughter}")
+        print(f"total:         {total}")
+        print(f"ratio:         {ratio:.4f}")
+        print(f"percentage:    {percentage:.2f}%")
         print()
+
+
+def plot(combined_list):
+    hour_list = [value_list[0] for value_list in combined_list]
+    laughter_list = [value_list[1] for value_list in combined_list]
+    total_list = [value_list[2] for value_list in combined_list]
+    ratio_list = [laughter_list[i]/total_list[i] for i in range(len(laughter_list))]
+    percentage_list = [laughter_list[i]/total_list[i]*100 for i in range(len(laughter_list))]
+    plt.plot(hour_list, ratio_list)
+    plt.xlabel('hour')
+    plt.ylabel('ratio')
+    plt.title('Ratio of tweets with laughter vs total tweets plotted against time')
+    plt.show()
+
+
+def print_results(combined_list, mode):
+    if mode == "pure":
+        print_pure(combined_list)
+    elif mode == "extended":
+        print_extended(combined_list)
+    elif mode == "plot":
+        plot(combined_list)
+    else:
+        print("Something went wrong...")
+        exit(-1)
+
+
+def main():
+    arg_check(sys.argv)
+
+    filenames = open_filenames(sys.argv[1])
+
+    chunks = open_files(filenames)
+
+    value_list_list = [split_into_values(chunk) for chunk in chunks]
+
+    combined_values = combine_values(value_list_list)
+
+    print_results(combined_values, sys.argv[2])
 
 
 if __name__ == "__main__":
